7f336be1e07eb43fb2276f032c481303452259f2
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-12-06 11:08:50 +0500

    убраны переменные all_ships и alive_ships, теперь просто ships

--- logic.py
+++ logic.py
@@ -18,8 +18,8 @@ class Game:
             self.xs = list('абвгдежзик')
         if ys is None:
             self.ys = list(range(1, 11))
-        self.all_ships = []
-        self.rest_ships = dict(ships_config)
+        self.ships = []                         # список всех кораблей, которые могут стоять на поле. Из этого списка убираем по сигналу "промах".
+        self.rest_ships = dict(ships_config)    # количество кораблей разного размера, которые где-то на поле.
         self.misses = []
         self.deads = []
         for size, count in ships_config:
@@ -29,22 +29,20 @@ class Game:
                     for offset in range(size):
                         if x + offset <= len(self.xs):
                             this_ship.append((x + offset, y))
-
-                    self.all_ships.append(this_ship)
+                    self.ships.append(this_ship)
             if size > 1:
                 for x, _ in enumerate(self.xs):
                     for y in range(len(self.ys) - size + 1):
                         this_ship = []
                         for offset in range(size):
                             this_ship.append((x, y + offset))
-                        self.all_ships.append(this_ship)
-        self.alive_ships = self.all_ships.copy()
+                        self.ships.append(this_ship)
         self.misses = []  # тут отмечаем все промахи по чужому полю
         self.wounds = []  # тут отмечаем все раненые ячейки
 
     def show_field(self, scheme_name):
         c = Counter()
-        for ship in self.alive_ships:
+        for ship in self.ships:
             c.update(ship)
         ret = {}
         for x, _ in enumerate(self.xs):
@@ -64,7 +62,7 @@ class Game:
     def choise_shot(self):
         ships_to_shot = []
 
-        for ship in self.alive_ships:
+        for ship in self.ships:
             if self.wounds:
                 if all(cell in ship for cell in self.wounds):
                     ships_to_shot.append(ship)
@@ -82,11 +80,11 @@ class Game:
     def miss(self, xy):
         self.misses.append(xy)
         ships_to_remove = []
-        for ship in self.alive_ships:
+        for ship in self.ships:
             if xy in ship:
                 ships_to_remove.append(ship)
         for ship in ships_to_remove:
-            self.alive_ships.remove(ship)
+            self.ships.remove(ship)
 
     def wound(self, xy):
         self.deads.append(xy)
@@ -103,12 +101,12 @@ class Game:
             forbiden_cells.append(cell)
             for dx, dy in itertools.product([-1, 0, 1], [-1, 0, 1]):
                 forbiden_cells.append((cell[0] + dx, cell[1] + dy))
-        for ship in self.alive_ships:
+        for ship in self.ships:
             if any(cell in forbiden_cells for cell in ship):
                 ships_to_remove.append(ship)
             if self.rest_ships[sank_ship_size] == 0 and len(ship) == sank_ship_size:
                 ships_to_remove.append(ship)
         for ship in ships_to_remove:
-            if ship in self.alive_ships:
-                self.alive_ships.remove(ship)
+            if ship in self.ships:
+                self.ships.remove(ship)
         self.wounds = []
